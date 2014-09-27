fa9b4ad62a183835b8b019d59bb12697ef6b78fc
Rich Lowenberg <me@richlowenberg.com>
2014-09-27 14:00:47 -0700

    Added confidence label

--- Handy/Handy/ViewController.swift
+++ Handy/Handy/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet var resultLabel:UILabel?;
+    @IBOutlet var confidenceLabel:UILabel?;
 
 	let handy = HandyGeometry()
     
